18574f2f3260b39a5fafb56e0b2a33b96d006877
ULAKHABERLESME\jale.arslan <jale.arslan@ulakhaberlesme.com.tr>
2019-09-10 15:50:59 +0300

    ChromeHeadless

--- jale-test/src/karma.conf.js
+++ jale-test/src/karma.conf.js
@@ -4,7 +4,7 @@
 module.exports = function(config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine','@angular-devkit/build-angular'],
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
@@ -15,31 +15,17 @@ module.exports = function(config) {
     client: {
       clearContext: false,
     },
-    files: [
-      { pattern: './test.ts', watched: false }
-    ],
-    preprocessors: {
-      './test.ts': ['@angular-devkit/build-angular']
-    },
-    mime: {
-      'text/x-typescript': ['ts', 'tsx']
-    },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../coverage/jale-test'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true,
     },
-    angularCli: {
-      environment: 'dev'
-    },
-    reporters: config.angularCli && config.angularCli.codeCoverage
-      ? ['progress', 'coverage-istanbul']
-      : ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['PhantomJS'],
+    browsers: ['ChromeHeadless'],
     singleRun: false,
     restartOnFileChange: true,
   });
