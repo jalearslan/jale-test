15f108ff118ca69ae2f01f596f03682cab8676ae
ULAKHABERLESME\jale.arslan <jale.arslan@ulakhaberlesme.com.tr>
2019-09-10 14:14:17 +0300

    change karma.config

--- jale-test/src/karma.conf.js
+++ jale-test/src/karma.conf.js
@@ -1,41 +1,54 @@
 // Karma configuration file, see link for more information
-// https://karma-runner.github.io/1.0/config/configuration-file.html
+// https://karma-runner.github.io/0.13/config/configuration-file.html
 
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['jasmine', '@angular/cli'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-phantomjs-launcher'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma'),
+      require('@angular/cli/plugins/karma')
     ],
-    client: {
-      clearContext: false,
+    client:{
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    files: [
+      // { pattern: './node_modules/jquery/dist/jquery.min.js', watched: false },
+      // { pattern: './node_modules/i18next/i18next.min.js', watched: false },
+      // { pattern: './node_modules/jquery-i18next/jquery-i18next.js', watched: false },
+      // { pattern: './src/assets/netClustering.js', watched: false },
+      { pattern: './src/test.ts', watched: false }
+    ],
+    preprocessors: {
+      './src/test.ts': ['@angular/cli']
+    },
+    mime: {
+      'text/x-typescript': ['ts','tsx']
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/jale-test'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+      reports: [ 'html', 'lcovonly' ],
       fixWebpackSourcePaths: true,
+      thresholds: {
+        statements: 70,
+        lines: 70,
+        branches: 70,
+        functions: 70
+      }
     },
-    // files: [ { pattern: './test.ts', watched: false }],
-      // pattern:['./node_modules/phantomjs-polyfill-object-assign/object-assign-polyfill.js']},
+    angularCli: {
+      environment: 'dev'
+    },
     reporters: config.angularCli && config.angularCli.codeCoverage
       ? ['progress', 'coverage-istanbul']
       : ['progress', 'kjhtml'],
-    junitReporter: {
-      outputDir: 'karma-results',
-      outputFile: 'karma-results.xml'
-    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: [ 'PhantomJS' ],
-    singleRun: false,
-    restartOnFileChange: true,
+    browsers: ['Chrome'],
+    singleRun: false
   });
 };
