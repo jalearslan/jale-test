c7f8463af073a71aa264561cc638501e528a72c6
ULAKHABERLESME\jale.arslan <jale.arslan@ulakhaberlesme.com.tr>
2019-09-10 14:20:53 +0300

    zz

--- jale-test/src/karma.conf.js
+++ jale-test/src/karma.conf.js
@@ -1,7 +1,7 @@
 // Karma configuration file, see link for more information
-// https://karma-runner.github.io/0.13/config/configuration-file.html
+// https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
@@ -10,45 +10,23 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('@angular-devkit/build-angular/plugins/karma'),
     ],
-    client:{
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    files: [
-      // { pattern: './node_modules/jquery/dist/jquery.min.js', watched: false },
-      // { pattern: './node_modules/i18next/i18next.min.js', watched: false },
-      // { pattern: './node_modules/jquery-i18next/jquery-i18next.js', watched: false },
-      // { pattern: './src/assets/netClustering.js', watched: false },
-      { pattern: './test.ts', watched: false }
-    ],
-    preprocessors: {
-      './src/test.ts': ['@angular-devkit/build-angular']
-    },
-    mime: {
-      'text/x-typescript': ['ts','tsx']
+    client: {
+      clearContext: false,
     },
     coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
+      dir: require('path').join(__dirname, '../coverage/jale-test'),
+      reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true,
-      thresholds: {
-        statements: 70,
-        lines: 70,
-        branches: 70,
-        functions: 70
-      }
     },
-    angularCli: {
-      environment: 'dev'
-    },
-    reporters: config.angularCli && config.angularCli.codeCoverage
-      ? ['progress', 'coverage-istanbul']
-      : ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: false,
+    restartOnFileChange: true,
   });
 };
